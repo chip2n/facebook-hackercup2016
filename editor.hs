3f336e447e2dc07f812bb8868f2cb61aca6b82a2
Andreas Arvidsson <andreas.arvidson@gmail.com>
2016-01-11 03:07:02 +0100

    Add Haskell solution to the editor problem.

---
+++ editor.hs
@@ -0,0 +1,70 @@
+import Data.List
+import Control.Monad
+import Control.Applicative hiding (many, (<|>))
+import Text.Parsec hiding (getInput)
+import Text.Parsec.String
+import System.Environment
+
+main :: IO()
+main = do
+    args <- getArgs
+    solve (head args)
+
+inputFile :: Parser [([String], Int)]
+inputFile = do
+  input <- many chunk
+  eof
+  return input
+
+chunk :: Parser ([String], Int)
+chunk = do
+  (c, a) <- infoLine 
+  result <- count c line
+  return (result, a)
+
+infoLine :: Parser (Int, Int)
+infoLine = do
+    [x, y] <- number `sepBy` (char ' ' *> spaces)
+    try $ char '\n'
+    return (x,y)
+
+line :: Parser String
+line = do
+    l <- many $ noneOf "\n"
+    try $ char '\n'
+    return l
+
+number = read <$> many digit
+
+getInput :: FilePath -> IO [([String], Int)]
+getInput fp = do
+    contents <- liftM lines $ readFile fp
+    let chunkCount = head contents
+        contents' = tail contents
+    case parse inputFile "(unknown)" (unlines contents') of
+        Right x -> return x
+        Left x -> error $ show x
+
+solve :: FilePath -> IO ()
+solve fp = do
+    input <- getInput fp
+    putStrLn $ unlines $ answers input
+  where render n value = "Case #" ++ show n ++ ": " ++ show value
+        answers input = map (\(n, a) -> render n $ uncurry solve2 a) (zip [1..] input)
+
+wordDiff :: String -> String -> Int
+wordDiff (x:xs) (y:ys) | x == y = wordDiff xs ys
+wordDiff xs     ys              = length xs + length ys
+
+cheapestNextWord :: String -> [String] -> (String, Int)
+cheapestNextWord word words = minimumBy lowestCost costs
+  where words' = delete word words
+        lowestCost (_, a) (_, b) = compare a b
+        costs = map (\w -> (w, wordDiff word w)) words'
+
+solve2 :: [String] -> Int -> Int
+solve2 words count = minimum $ map (\w -> solve' words count (length w) w) words
+  where solve' _     1     cost word = 1 + cost + length word
+        solve' words count cost word = let words' = delete word words
+                                           (nextWord, cost') = cheapestNextWord word words
+                                       in  solve' words' (count - 1) (1 + cost + cost') nextWord
